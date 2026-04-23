c3a154459084214b11da3ff16aaf875c7c82de13
Ball <fuxiaoball@gmail.com>
2026-04-24 07:05:53 +0800

    修改网站标题

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "蓝博航发研学基地",
-  description: "蓝博航发研学基地 3D打印创客空间官方网站",
+  title: "泸州航发3D智造空间",
+  description: "泸州航发3D智造空间 AI+3D打印研学创新平台",
 };
 
 export default function RootLayout({ children }) {
